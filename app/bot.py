4abe80ec5117d7e9437aeb3fd73b88a8c6ef4cce
Rouven <rouven@rfive.de>
2022-07-23 10:35:22 +0200

    fix duplicate logging

--- app/bot.py
+++ app/bot.py
@@ -41,6 +41,7 @@ if "--debug" in sys.argv:
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+logger.handlers.clear()
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter("%(levelname)s [%(module)s.%(funcName)s]: %(message)s"))
 logger.addHandler(console_handler)
