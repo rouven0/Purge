82a64f778ee5edb2af95236077afef237a150104
Rouven Seifert <rouven@rfive.de>
2023-05-02 00:28:47 +0200

    remove unsupported architectures

--- flake.nix
+++ flake.nix
@@ -4,7 +4,8 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      # supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
