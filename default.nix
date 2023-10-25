85e9a0b24e04689f813283c9806194f18352939c
Rouven Seifert <rouven@rfive.de>
2023-10-26 00:45:29 +0200

    nix: use upstream quart

--- default.nix
+++ default.nix
@@ -19,25 +19,8 @@ buildPythonPackage {
           requests-toolbelt
           pynacl
           pytest
-          (buildPythonPackage
-            rec {
-              pname = "quart";
-              version = "0.18.4";
-              propagatedBuildInputs = [
-                flask
-                hypercorn
-                markupsafe
-                blinker
-                aiofiles
-              ];
-
-              src = fetchPypi {
-                inherit pname version;
-                sha256 = "wXZvJpzbhdr52me6VBcKv3g5rKlzBNy0zQd46r+0QsY=";
-              };
-            })
+          quart
         ];
-
         src = fetchPypi {
           inherit pname version;
           sha256 = "3jN0RcArARN1nt6pZTPQS7ZglFUE17ZSpLcsOX49gLM=";
